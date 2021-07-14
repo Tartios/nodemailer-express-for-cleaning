59dccb392033545a666c7378671ba9b92294ca84
Tartios <tartios@yandex.ru>
2021-07-14 20:29:13 +0300

    upd

--- Nodemailer.js
+++ Nodemailer.js
@@ -9,29 +9,15 @@ const transport = {
         password: creds.PASSWORD
     },
 }
-
 const transporter = nodemailer.createTransport(transport);
 
-transporter.verify((error, success) => {
-    if (error) {
-        console.log(error);
-    }
-    console.log('Server is ready to take messages');
-});
-
-router.post('/send', (req, res, next) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message;
-    const content = `name: ${name} \n email: ${email} \n message: ${message}`;
-    
-    const mail = {
-        from: name,
-        to: 'tartios@yandex.ru',
-        subject: 'ТЕМА ПИСЬМА',
-        text: content,
-    }
-
+// transporter.verify((error, success) => {
+//     if (error) {
+//         console.log(error);
+//     }
+//     console.log('Server is ready to take messages');
+// });
+const postPush = () => {
     transporter.sendMail(mail, (err, data) => {
         if (err) {
             res.json({
@@ -42,5 +28,6 @@ router.post('/send', (req, res, next) => {
             status: 'ok',
         })
     })
+}
 
-});
+module.exports = postPush;
--- index.js
+++ index.js
@@ -3,9 +3,25 @@ const http = require('http');
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
-
+const postman = require('./Nodemailer');
 const app = express()
 app.use(cors());
 app.use(express.json());
+//отдельный модуль
+router.post('/send', (req, res, next) => {
+    console.log(req);
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+    const content = `name: ${name} \n email: ${email} \n message: ${message}`;
+    
+    const mail = {
+        from: name,
+        to: 'tartios@yandex.ru',
+        subject: 'ТЕМА ПИСЬМА',
+        text: content,
+    }
+});
+//вот это вот все
 app.use('/', router);
 app.listen(3002)
